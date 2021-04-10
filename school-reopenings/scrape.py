9e9d53737bef3cf568c228aab73fb172f4e40940
Iris Lee <lee.iris.s@gmail.com>
2021-04-09 18:16:35 -0700

    move and edit some

--- school-reopenings/scrape.py
+++ school-reopenings/scrape.py
@@ -12,9 +12,9 @@ def main():
 
     query="query?f=json&returnGeometry=false&outFields=*&where=1=1"
 
-    url_list={"districts":"https://services3.arcgis.com/uknczv4rpevve42E/ArcGIS/rest/services/Reopening_Status_to_Districts_FGDB/FeatureServer/0/",
-        "charters":"https://services3.arcgis.com/uknczv4rpevve42E/ArcGIS/rest/services/Join_Reopening_Status_to_Charters_FGDB/FeatureServer/0/",
-        "private":"https://services3.arcgis.com/uknczv4rpevve42E/ArcGIS/rest/services/Join_Reopening_Status_to_Private_Schools_FGDB/FeatureServer/0/"}
+    url_list={"districts":"https://services3.arcgis.com/uknczv4rpevve42E/arcgis/rest/services/CA_Trailer_Bill_Districts_Snapshot_Stg/FeatureServer/0/",
+        "charters":"https://services3.arcgis.com/uknczv4rpevve42E/ArcGIS/rest/services/CA_Trailer_Bill_Charter_Schools_Snapshot_Stg/FeatureServer/0/",
+        "private":"https://services3.arcgis.com/uknczv4rpevve42E/ArcGIS/rest/services/CA_Trailer_Bill_Private_Schools_Snapshot_Stg/FeatureServer/0/"}
 
     for slug, u in url_list.items():
         url = u+query
@@ -29,4 +29,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# NEW LINKS AS OF 4/9/2021 - Old links below
+#url_list={"districts":"https://services3.arcgis.com/uknczv4rpevve42E/ArcGIS/rest/services/Reopening_Status_to_Districts_FGDB/FeatureServer/0/",
+        #"charters":"https://services3.arcgis.com/uknczv4rpevve42E/ArcGIS/rest/services/Join_Reopening_Status_to_Charters_FGDB/FeatureServer/0/",
+        #"private":"https://services3.arcgis.com/uknczv4rpevve42E/ArcGIS/rest/services/Join_Reopening_Status_to_Private_Schools_FGDB/FeatureServer/0/"}
+
     
