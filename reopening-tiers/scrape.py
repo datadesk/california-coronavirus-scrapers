552e47dcaff6d2b16fb828bf56c631f8458c1a4e
palewire <b@palewi.re>
2021-04-26 08:45:40 -0700

    Datestamp it too

--- reopening-tiers/scrape.py
+++ reopening-tiers/scrape.py
@@ -3,8 +3,10 @@ Download the status of each county according to California's tier-based reopenin
 
 Source: https://covid19.ca.gov/safer-economy/
 """
+import pytz
 import pathlib
 import pandas as pd
+from datetime import datetime
 
 # Pathing
 THIS_DIR = pathlib.Path(__file__).parent.absolute()
@@ -20,6 +22,10 @@ def main():
     df = pd.read_json(url)
     # Save it to the raw data folder
     df.to_csv(DATA_DIR / "latest.csv", index=False)
+    # Save it datestamped now too
+    tz = pytz.timezone("America/Los_Angeles")
+    today = datetime.now(tz).date()
+    df.to_csv(DATA_DIR / f"{today}.csv", index=False)
 
 
 if __name__ == '__main__':
