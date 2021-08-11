fab1f2948b465df4e926d0205e46e6fa1ecf2033
Sean Greene <2756496+seangreene@users.noreply.github.com>
2021-08-11 14:56:29 -0700

    fix endpoint

--- hospital-capacity/scrape.py
+++ hospital-capacity/scrape.py
@@ -14,7 +14,7 @@ def main():
     """
     Download the metadata endpoint and find the CSV.
     """
-    url = "https://beta.healthdata.gov/api/views/anag-cw7u/rows.csv?accessType=DOWNLOAD"
+    url = "https://healthdata.gov/api/views/anag-cw7u/rows.csv?accessType=DOWNLOAD"
     r = requests.get(url)
     data = r.content.decode("utf-8")
     df = pd.read_csv(io.StringIO(data), dtype={"fips_code": str, "zip": str})
