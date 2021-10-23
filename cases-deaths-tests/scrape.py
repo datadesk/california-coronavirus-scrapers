349c78095a578ca4419754df7339825cc36f256b
palewire <b@palewi.re>
2021-10-23 09:20:20 -0700

    Lowercase cases-deaths-tests headers for #194

--- cases-deaths-tests/scrape.py
+++ cases-deaths-tests/scrape.py
@@ -18,6 +18,8 @@ def main():
     # Download the data
     url = "https://data.chhs.ca.gov/dataset/f333528b-4d38-4814-bebb-12db1f10f535/resource/046cdd2b-31e5-4d34-9ed3-b48cdbc4be7a/download/covid19cases_test.csv"
     df = pd.read_csv(url)
+    # Lower case the column headers, since the state likes to jack around with this for no reason
+    df.columns = map(str.lower, df.columns)
     # Save it to the data folder
     df.to_csv(DATA_DIR / "latest.csv", index=False)
 
