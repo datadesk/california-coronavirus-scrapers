6b82642c394fe8e5bc1df33be23e2ae782e5fa93
Sean Greene <seangreene89@gmail.com>
2022-11-03 17:01:05 -0700

    new urls for vax by demo

--- vaccine-demographics-statewide/scrape.py
+++ vaccine-demographics-statewide/scrape.py
@@ -1,7 +1,7 @@
 """
 Download the California Department of Public Health's dataset of vaccine doses administered by age, gender and race/ethnicity.
 
-Source: https://data.ca.gov/dataset/covid-19-vaccine-progress-dashboard-data/resource/9a0b200c-5a62-4545-912a-ea3f9e5b7c76
+Source: https://data.ca.gov/dataset/covid-19-vaccine-progress-dashboard-data
 """
 import pathlib
 import pandas as pd
