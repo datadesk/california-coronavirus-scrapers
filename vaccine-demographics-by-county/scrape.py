0ef871d8994eeec3e6976d3badfa3b85f356a940
Sean Greene <seangreene89@gmail.com>
2022-11-03 21:03:04 -0700

    split files into two CSVs

--- vaccine-demographics-by-county/scrape.py
+++ vaccine-demographics-by-county/scrape.py
@@ -18,8 +18,17 @@ def main():
     # Download the data
     url = "https://data.chhs.ca.gov/dataset/e283ee5a-cf18-4f20-a92c-ee94a2866ccd/resource/71729331-2f09-4ea4-a52f-a2661972e146/download/covid19vaccinesbycountybydemographic.csv"
     df = pd.read_csv(url)
+    
+    # split dataframes into two because of large file size
+    
+    age_df = df[ df["demographic_category"] == "Age Group" ]
+    
+    race_df =  df[ df["demographic_category"] == "Race/Ethnicity" ]
+    
     # Save it to the raw data folder
-    df.to_csv(DATA_DIR / "latest.csv", index=False)
+    age_df.to_csv(DATA_DIR / "latest-age-group.csv", index=False)
+    
+    race_df.to_csv(DATA_DIR / "latest-race-ethnicity.csv", index=False)
 
 
 if __name__ == '__main__':
