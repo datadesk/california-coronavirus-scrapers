961745bc423235d43dd92e0bc3681b893a07c6dd
Iris Lee <lee.iris.s@gmail.com>
2023-02-21 15:51:45 -0800

    new link

--- hospital-locations/scrape.py
+++ hospital-locations/scrape.py
@@ -12,8 +12,10 @@ DATA_DIR = THIS_DIR / "data"
 def main():
     """
     Download the metadata endpoint and find the CSV.
+    
     """
-    url = "https://opendata.arcgis.com/datasets/9f70e309bb324d3f96c49a3ead7be776_0.csv"
+  
+    url = "https://opendata.arcgis.com/api/v3/datasets/05b975ad01b842e18d0b02aa655632cb_0/downloads/data?format=csv&spatialRefId=4326&where=1%3D1"
     df = pd.read_csv(url, dtype={"CCN": str, "Zip_Code": str})
     df.to_csv(DATA_DIR / "latest.csv", index=False)
 
